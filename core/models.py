72e249c842d5f19e86d142fced7a75fd3890fdda
Walter Licitra <waltercoder84735@gmail.com>
2026-04-19 20:55:15 -0300

    CRUD artículos completo, formato de contenido corregido

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Servicio(models.Model):
     nombre = models.CharField(max_length=100)
@@ -12,7 +13,7 @@ class Servicio(models.Model):
 class Articulo(models.Model):
     titulo = models.CharField(max_length=200)
     tipo = models.CharField(max_length=20)
-    contenido = models.TextField()
+    contenido = RichTextField()
     imagen = models.ImageField(upload_to='articulos/', null=True, blank=True)
     fecha_publicacion = models.DateField(auto_now_add=True)
 
